def5f6dd1fec925683511179e94ac39d7a9d77b1
Pakki Pavan <pavanpakki786@gmail.com>
2021-08-22 13:07:50 +0530

    json

--- express/server.js
+++ express/server.js
@@ -7,6 +7,11 @@ const bodyParser = require('body-parser');
 
 const router = express.Router();
 router.get('/', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('<h1>Hello from Express.js!</h1>');
+  res.end();
+});
+router.get('/test', (req, res) => {
 	res.json({
 		abc:123,
 		def:456
