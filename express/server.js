a2b3389a183f596244b7bf154c3ada0c096b0470
Pakki Pavan <pavanpakki786@gmail.com>
2021-08-22 13:02:12 +0530

    json

--- express/server.js
+++ express/server.js
@@ -7,9 +7,13 @@ const bodyParser = require('body-parser');
 
 const router = express.Router();
 router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
-  res.end();
+	res.json({
+		abc:123,
+		def:456
+	});
+  //res.writeHead(200, { 'Content-Type': 'text/html' });
+  //res.write('<h1>Hello from Express.js!</h1>');
+  //res.end();
 });
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
